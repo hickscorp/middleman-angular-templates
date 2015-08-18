7664e72e24f83f5544a014b045295a8f7a585ca6
HERAULT-MARTIN Pierre <HicksCorp@GMail.com>
2015-08-18 20:18:29 +0100

    Attemps to fix the partial lock.

--- lib/middleman-angular-templates.rb
+++ lib/middleman-angular-templates.rb
@@ -8,13 +8,13 @@ class AngularTemplates < ::Middleman::Extension
   end
 
   helpers do
-    def angular_include_templates(path = '')
+    def angular_include_templates
       base_dir = Pathname.new(@@base_dir || config.source)
       source_dir = Pathname.new(config.source)
       ''.tap do |html|
-        Dir[File.join(base_dir, path, '**/*.{html,erb,slim,haml}')].each do |filename|
+        Dir[File.join( base_dir, 'templates', '**/*.{html,erb,slim,haml}')].each do |filename|
           template_name = File.basename(filename, '.*')
-          next unless template_name.slice!(0) == '_'
+          #next unless template_name.slice!(0) == '_'
           template_path = Pathname.new(File.join(File.dirname(filename), template_name))
           template_id = template_path.relative_path_from(base_dir)
           html << content_tag(
