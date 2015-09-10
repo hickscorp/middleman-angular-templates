bcf5466acc79118bd8ab8ccba902a8967ef4561a
HERAULT-MARTIN Pierre <HicksCorp@GMail.com>
2015-09-10 12:54:13 +0100

    Adds the slash back...

--- lib/middleman-angular-templates.rb
+++ lib/middleman-angular-templates.rb
@@ -21,13 +21,12 @@ class AngularTemplates < ::Middleman::Extension
             :script,
             partial(template_path.relative_path_from(source_dir)),
             type: "text/ng-template",
-            id: "#{template_id}.html"
+            id: "/#{template_id}.html"
           )
         end
       end
     end
   end
-
 end
 
 AngularTemplates.register(:angular_templates)
